e686d76e3689ed57223d1eecdd5b3ce276d5c1c5
Will Montiel <will.montiel@aol.com>
2017-04-06 00:27:29 -0500

    Remove autocomplete service code

--- app/src/main/java/com/development/transportesejecutivos/fragments/DestinyFragment.java
+++ app/src/main/java/com/development/transportesejecutivos/fragments/DestinyFragment.java
@@ -1,9 +1,6 @@
 package com.development.transportesejecutivos.fragments;
 
-import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
@@ -16,9 +13,6 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 import com.development.transportesejecutivos.R;
-import com.development.transportesejecutivos.RequestserviceActivity;
-import com.development.transportesejecutivos.ServicerequestActivity;
-import com.development.transportesejecutivos.adapters.JsonKeys;
 import com.development.transportesejecutivos.misc.PlaceArrayAdapter;
 import com.development.transportesejecutivos.models.User;
 import com.google.android.gms.common.ConnectionResult;
